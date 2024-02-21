ed4566698e1cf37b6c9a837cb94c626a2a2da9ac
cw-gtanchak <gtanchak@codewinglet.com>
2024-02-21 16:53:27 +0530

    setup storybook and color pallete

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { useState } from "react";
-import { Button } from "~/components/ui/button";
+import { Button } from "~/components/ui/Button/Button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -18,5 +18,9 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   const [count, setCount] = useState(0);
-  return <Button onClick={() => setCount(count + 1)}>Home {count}</Button>;
+  return (
+    <Button variant="default" onClick={() => setCount(count + 1)}>
+      Home {count}
+    </Button>
+  );
 }
